acacd831e462714b8e2f4f763901cf26abd81a21
strongtools <87340841+strongtools@users.noreply.github.com>
2022-04-25 08:15:40 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let bronzeNft = document.getElementById("bronze-nft")
 let coinstMined = document.getElementById("coins-mined")
 const strongPrice = document.querySelector(".strong-price")
 const strongIcon = document.querySelector(".strong-icon")
-const strongAPI = "https://api.coingecko.com/api/v3/coins/strong"
+const strongAPI = "https://api.coingecko.com/api/v3/coins/stronger"
 
 button.addEventListener("click", calculate)
 
@@ -27,30 +27,60 @@ function calculate() {
     let mined = coinstMined.value
     let dailyToken = ((current *0.0924) + (platinum * 0.464) + (gold * 0.185) + (silver * 0.055) + (bronze * 0.0125) + (mined * 0.001369)) 
     let dailyPrice = dailyToken * price 
+    // let days = 0;
+    //     for(let i=current; i<wanted; i++) {
+    //     let math = days += 10/((i*0.0924) + (platinum * 0.464) + (gold * 0.185) + (silver * 0.055) + (bronze * 0.0125) + (mined * 0.001369))    
+    //     let newContent = `
+    //         <p>It will take you: <span class="span-color">${math.toFixed(2)}</span> days.</p>
+    //         <p>Your amount of daily tokens: <span class="span-color">${dailyToken.toFixed(4)}</span></p>
+    //         <p>Your daily income is : <span class="span-color">$${dailyPrice.toFixed(2)}</span></p>
+    //         `
+    //     displayArea.innerHTML = newContent   
+    //     console.log(math)
+         
+    // }
+
+
     let days = 0;
         for(let i=current; i<wanted; i++) {
-        let math = (days += 10/(i*0.0924 + (platinum * 0.464) + (gold * 0.185) + (silver * 0.055) + (bronze * 0.0125) + (mined * 0.001369)))        
+        days += 10/((i*.0924))    
         let newContent = `
-        <p>It will take you: <span class="span-color">${math.toFixed(2)}</span> days.</p>
-        <p>Your amount of daily tokens: <span class="span-color">${dailyToken.toFixed(4)}</span></p>
-        <p>Your daily income is : <span class="span-color">$${dailyPrice.toFixed(2)}</span></p>
-        `
-        displayArea.innerHTML = newContent 
-        console.log(newContent)     
+            <p>It will take you: <span class="span-color">${days}}</span> days.</p>
+            <p>Your amount of daily tokens: <span class="span-color">${dailyToken.toFixed(4)}</span></p>
+            <p>Your daily income is : <span class="span-color">$${dailyPrice.toFixed(2)}</span></p>
+            `
+        displayArea.innerHTML = newContent   
+        console.log(days)
+         
     }
+
+//         let days = 0
+// const sumOf = (current, wanted) => {
+//         for (let i=current; i<wanted; i++) {
+//             days += 10/((i*0.0924))
+//         }
+//         return (days)
+//         }
+
     
+    console.log(days);
+    console.log(current);
+    console.log(wanted)
     console.log(days.toFixed(2))
     console.log(mined)
+    console.log(dailyToken)
     console.log(dailyPrice)
-    if (wanted == 0 || current === wanted) {
-        displayArea.innerHTML = `
-        <p>Your amount of daily tokens: <span class="span-color">${dailyToken.toFixed(4)}</span></p>
-        <p>Your daily income is : <span class="span-color">$${dailyPrice.toFixed(2)}</span></p>
-        `
-    } else if (current > wanted) {
-        displayArea.innerHTML = "Current value is higher than wanted"
-    }
+        // if (wanted == 0 || current === wanted) {
+        //     displayArea.innerHTML = `
+        //     <p>Your amount of daily tokens: <span class="span-color">${dailyToken.toFixed(4)}</span></p>
+        //     <p>Your daily income is : <span class="span-color">$${dailyPrice.toFixed(2)}</span></p>
+        //     `
+        // } else if (current > wanted) {
+        //     displayArea.innerHTML = "Current value is higher than wanted"
+        // }  
 }
+
+
 
 //API FETCH STRONG INFORMATION FUNCTION
 let variablePrice;
